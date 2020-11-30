ccd350f190a27018e4c460c27f897d540ba46492
Noga Shilo <73280078+NogaShilo@users.noreply.github.com>
2020-11-30 10:50:00 +0100

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,5 +1,5 @@
 
-(function () {
+(function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -14,5 +14,7 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker.speak(names[i]);
   }
 }
+   window.byeSpeaker = byeSpeaker;
+   window.helloSpeaker = helloSpeaker;
 
-})();
+})(window);
